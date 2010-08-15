c0ad78da322251ab59fc6d1d49966268af26bcde
henning <henning@b35dd754-fafc-0310-a699-88a17e54d16e>
2010-08-15 16:32:09 +0000

    add the ability to register for roster events before logging in (SMACK-156)
    git-svn-id: http://svn.igniterealtime.org/svn/repos/smack/trunk@11826 b35dd754-fafc-0310-a699-88a17e54d16e

--- source/org/jivesoftware/smack/Roster.java
+++ source/org/jivesoftware/smack/Roster.java
@@ -63,7 +63,7 @@ public class Roster {
     private final List<RosterListener> rosterListeners;
     private Map<String, Map<String, Presence>> presenceMap;
     // The roster is marked as initialized when at least a single roster packet
-    // has been recieved and processed.
+    // has been received and processed.
     boolean rosterInitialized = false;
     private PresencePacketListener presencePacketListener;
 
@@ -123,8 +123,10 @@ public class Roster {
         PacketFilter presenceFilter = new PacketTypeFilter(Presence.class);
         presencePacketListener = new PresencePacketListener();
         connection.addPacketListener(presencePacketListener, presenceFilter);
+        
         // Listen for connection events
-        connection.addConnectionListener(new ConnectionListener() {
+        final ConnectionListener connectionListener = new AbstractConnectionListener() {
+            
             public void connectionClosed() {
                 // Changes the presence available contacts to unavailable
                 setOfflinePresences();
@@ -135,19 +137,22 @@ public class Roster {
                 setOfflinePresences();
             }
 
-            public void reconnectingIn(int seconds) {
-                // Ignore
-            }
-
-            public void reconnectionFailed(Exception e) {
-                // Ignore
-            }
-
-            public void reconnectionSuccessful() {
-                // Ignore
-            }
-        });
+        };
         
+        // if not connected add listener after successful login
+        if(!this.connection.isConnected()) {
+            Connection.addConnectionCreationListener(new ConnectionCreationListener() {
+                
+                public void connectionCreated(Connection connection) {
+                    if(connection.equals(Roster.this.connection)) {
+                        Roster.this.connection.addConnectionListener(connectionListener);
+                    }
+                    
+                }
+            });
+        } else {
+            connection.addConnectionListener(connectionListener);
+        }
     }
 
     /**
@@ -184,8 +189,17 @@ public class Roster {
      * Reloads the entire roster from the server. This is an asynchronous operation,
      * which means the method will return immediately, and the roster will be
      * reloaded at a later point when the server responds to the reload request.
+     * 
+     * @throws IllegalStateException if connection is not logged in or logged in anonymously
      */
     public void reload() {
+        if (!connection.isAuthenticated()) {
+            throw new IllegalStateException("Not logged in to server.");
+        }
+        if (connection.isAnonymous()) {
+            throw new IllegalStateException("Anonymous users can't have a roster.");
+        }
+
     	RosterPacket packet = new RosterPacket();
     	if(persistentStorage!=null){
     		packet.setVersion(persistentStorage.getRosterVersion());
@@ -226,11 +240,19 @@ public class Roster {
      *
      * @param name the name of the group.
      * @return a new group.
+     * @throws IllegalStateException if connection is not logged in or logged in anonymously
      */
     public RosterGroup createGroup(String name) {
+        if (!connection.isAuthenticated()) {
+            throw new IllegalStateException("Not logged in to server.");
+        }
+        if (connection.isAnonymous()) {
+            throw new IllegalStateException("Anonymous users can't have a roster.");
+        }
         if (groups.containsKey(name)) {
             throw new IllegalArgumentException("Group with name " + name + " alread exists.");
         }
+        
         RosterGroup group = new RosterGroup(name, connection);
         groups.put(name, group);
         return group;
@@ -245,8 +267,16 @@ public class Roster {
      * @param groups the list of group names the entry will belong to, or <tt>null</tt> if the
      *               the roster entry won't belong to a group.
      * @throws XMPPException if an XMPP exception occurs.
+     * @throws IllegalStateException if connection is not logged in or logged in anonymously
      */
     public void createEntry(String user, String name, String[] groups) throws XMPPException {
+        if (!connection.isAuthenticated()) {
+            throw new IllegalStateException("Not logged in to server.");
+        }
+        if (connection.isAnonymous()) {
+            throw new IllegalStateException("Anonymous users can't have a roster.");
+        }
+
         // Create and send roster entry creation packet.
         RosterPacket rosterPacket = new RosterPacket();
         rosterPacket.setType(IQ.Type.SET);
@@ -408,8 +438,16 @@ public class Roster {
      *
      * @param entry a roster entry.
      * @throws XMPPException if an XMPP error occurs.
+     * @throws IllegalStateException if connection is not logged in or logged in anonymously
      */
     public void removeEntry(RosterEntry entry) throws XMPPException {
+        if (!connection.isAuthenticated()) {
+            throw new IllegalStateException("Not logged in to server.");
+        }
+        if (connection.isAnonymous()) {
+            throw new IllegalStateException("Anonymous users can't have a roster.");
+        }
+
         // Only remove the entry if it's in the entry list.
         // The actual removal logic takes place in RosterPacketListenerprocess>>Packet(Packet)
         if (!entries.containsKey(entry.getUser())) {
@@ -530,7 +568,7 @@ public class Roster {
     }
 
     /**
-     * Returns an unmodiable collections of all the roster groups.
+     * Returns an unmodifiable collections of all the roster groups.
      *
      * @return an iterator for all roster groups.
      */
